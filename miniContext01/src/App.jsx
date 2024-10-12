d5451899bcf045e28511ae0cc5865178dbebaa6c
Mehtab Hussain <tradercoder786@gmail.com>
2024-10-12 20:31:40 +0530

    project 1

--- miniContext01/src/App.jsx
+++ miniContext01/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
+import UserContextProvider from './context/UserContextProvider'
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Starting Learning about context api
-    </h1>
-    </>
+    <UserContextProvider>
+    <h1 className="text-3xl font-bold underline"> Starting Learning about context api </h1>
+    <Login/>
+    <Profile/>
+    </UserContextProvider>
   )
 }
 
