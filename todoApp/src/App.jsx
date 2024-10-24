9fd727d77f9046a560888d4ed409cfb5edbc7316
Mehtab Hussain <tradercoder786@gmail.com>
2024-10-24 20:36:50 +0530

    features add

--- todoApp/src/App.jsx
+++ todoApp/src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function App() {
 
   const [task, setTask] = useState("")
   const [desc, setDesc] = useState("")
-  // const [currentTime, setCurrentTime] = useState("")
-  const [mainTask, setMainTask] = useState([])
-  // const [completed, setCompleted] = useState(false)
+  const [mainTask, setMainTask] = useState(() => {
+    const savedTask = localStorage.getItem('tasks');
+    return savedTask ? JSON.parse(savedTask) : []
+  })
+ 
 
-  
   
   const submitHandler = (e) => {
     e.preventDefault()
 
-    if (!task || !desc) {
-      alert("Please fill in both fields before submitting.");
-      return;
-    }
+    if (!task || !desc)  return alert("Please fill in both fields before submitting.");
+
+    if (mainTask.some(t => t.task === task)) return alert('Task already exists!')
+    
 
     const currentTime = new Date();
     const time = `${currentTime.getHours()}h:${currentTime.getMinutes()}m:${currentTime.getSeconds()}s`;
@@ -34,16 +35,12 @@ function App() {
     setMainTask(copiedTask)
   }
 
-  // const toggle = (idx) => {
-  //   const updatedTask = [...mainTask]
-  //   updatedTask[idx].completed = !updatedTask[idx].completed    
-  //   setMainTask(updatedTask)
-  // }
   const toggle = (idx) => {
     const updatedTasks = [...mainTask];
     updatedTasks[idx].completed = !updatedTasks[idx].completed; // Toggle the completed status
     setMainTask(updatedTasks); // Update the mainTask state with the modified array
   };
+  
   let renderTask = <h2 className='text-center text-2xl'>No task added</h2>
   
   if (mainTask.length > 0) {
@@ -61,7 +58,7 @@ function App() {
                  {task.desc}
                  </h4>
                 </div>
-                <h4 className={`mx-20 text-white font-semibold py-2 px-4 rounded-lg shadow-lg hover:bg-gradient-to-r ${task.completed ? 'line-through text-gray-500' : ''}`}>{task.time}</h4>
+                <h4 className='mx-20 text-white font-semibold py-2 px-4 rounded-lg shadow-lg hover:bg-gradient-to-r'>{task.time}</h4>
 
                 <button className='bg-red-500 text-xl py-1 px-3 rounded-lg' onClick={() => {return deleteHandler(idx)}}>Delete</button>
 
@@ -69,14 +66,30 @@ function App() {
     })
   }
 
+  const reset = () => {
+    setMainTask([])
+  }
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(mainTask))
+  }, [mainTask])
+
   
   return (
    <>
-   <div className='w-full h-screen bg-neutral-800 text-blue-400'>
+   <div className='w-full h-screen bg-neutral-800 relative  text-blue-400'>
 
-    <h1 className='text-center  text-7xl text-gray-40'>iTask</h1>
+   <h1 className='text-center text-7xl text-zinc-300 font-semibold shadow-lg 
+               text-shadow-md py-1' style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)' }}>
+  iTask
+</h1>
 
-    <div className='w-4/5 mx-auto'>
+
+    <button className='absolute top-7 right-7 text-2xl text-white py-1 px-4 rounded-lg bg-red-500'
+      onClick={reset}
+    >Reset</button>
+
+    <div className='w-4/5 mx-auto mt-10'>
       <form  onSubmit={submitHandler} className='flex justify-around px-32 py-7' >
         <input 
           type="text"
