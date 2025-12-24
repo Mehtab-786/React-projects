2c741a59819b87bb2a36db23c1f114089fbf7ca0
Mehtab Hussain <tradercoder786@gmail.com>
2025-12-24 23:12:45 +0530

    useCart hook - local sync storage, sync across tabs

--- Custom_Hook/src/hooks/useCart.js
+++ Custom_Hook/src/hooks/useCart.js
@@ -1,75 +1,41 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useCart() {
+    // const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || [])   
     const [cart, setCart] = useState(() => {
         try {
-            let data = JSON.parse(localStorage.getItem('cart'))
-            return data ? data : [];
+            let data = localStorage.getItem('cart');
+            return data ? JSON.parse(data) : [];
         } catch (error) {
-            console.error("Failed to load cart from storage :: ", error);
+            console.error('Failed to load cart :: ', error);
             return [];
         }
     })
 
-    // persist cart to local storage
+    //local sync storage
     useEffect(() => {
         try {
             localStorage.setItem('cart', JSON.stringify(cart))
         } catch (error) {
-            console.log('Failed to save cart', error)
+            console.error('Failed to save cart :: ', error);
         }
-    }, [cart])
+    }, [cart]);
 
     //sync across tabs
     useEffect(() => {
         const handleStorage = (e) => {
-            if (e.key === 'cart') {
+            if (e.key == 'cart') {
                 try {
                     const newCart = JSON.parse(e.newValue || '[]')
                     setCart(newCart)
                 } catch (error) {
-                    console.error('Failed to parse cart :: ', error)
+                    console.log('Failed to parse cart ::', error)
                 }
             }
         }
-
-        window.addEventListener('storage', handleStorage)
-        return () => window.removeEventListener('storage', handleStorage)
+        window.addEventListener('storage',handleStorage)
+        return () =>  window.removeEventListener('storage',handleStorage)
     }, [])
 
-    const addToCart = (product) => {
-        setCart(currentOne => {
-            const existingItem = currentOne.find(item => item.id === product.id);
-            if (existingItem) {
-                return currentOne.map(item => item.id === product.id ? { item, quantity: item.quantity + 1 } : item)
-            }
-            return [...currentOne, { ...product, quantity: 1 }]
-        })
-    }
 
-    const removeFromCart = (productId) => {
-        setCart(currentOne => currentOne.filter(item => item.id !== productId))
-    }
-
-    const updateQuantity = (productId, quantity) => {
-        if (quantity < 1) return;
-        setCart(current => current.map(item => item.id == productId ? { ...item, quantity } : item))
-    }
-
-    //not required in react 19
-    const total = useMemo(() => {
-        return Number(cart.reduce((sum, item) => {
-            const itemTotal = item.price * (item.quantity || 0)
-            return sum + itemTotal
-        }, 0).toFixed(2))
-    }, [cart])
-
-
-    return {
-        cart,
-        addToCart,
-        removeFromCart,
-        updateQuantity,
-        total
-    }
 }
