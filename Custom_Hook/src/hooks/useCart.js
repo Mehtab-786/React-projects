13100bfe5ac3e84107163ebe8eec885a57248530
Mehtab Hussain <tradercoder786@gmail.com>
2025-12-25 13:03:05 +0530

    Progess custom hok

--- Custom_Hook/src/hooks/useCart.js
+++ Custom_Hook/src/hooks/useCart.js
@@ -33,9 +33,29 @@ export function useCart() {
                 }
             }
         }
-        window.addEventListener('storage',handleStorage)
-        return () =>  window.removeEventListener('storage',handleStorage)
+        window.addEventListener('storage', handleStorage)
+        return () => window.removeEventListener('storage', handleStorage)
     }, [])
+
+    //add to cart
+    const addToCart = (product) => {
+        setCart(cartItems => {
+            let existingItem = cartItems.find(item => item.id == product.id)
+            if (existingItem) {
+                return cartItems.map(item => item.id == product.id ? { ...item, quantity: item.quantity + 1 } : item)
+            }
+            return [...cartItems, { ...product, quantity: 1 }]
+        });
+    };
+
+    const removeFromCart = (productId) => setCart(cartItems => cartItems.filter(item => item.id != productId));
+
+    const updateQuantity = (productId, quantity) => {
+        if (quantity < 1) return;
+        setCart(cartItems => {
+            return cartItems.map(item => item.id == productId ? {...item, quantity} : item)
+        })
+    }
 
 
 }
