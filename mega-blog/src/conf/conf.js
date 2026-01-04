32a1bc5c1193b722a5911e523e3f914b0d538c5b
Mehtab Hussain <tradercoder786@gmail.com>
2026-01-04 20:36:58 +0530

    Setting up Appwrite

---
+++ mega-blog/src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteId: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
+
+// Export env vars as strings here to avoid import timing issues and non-string value errors.
