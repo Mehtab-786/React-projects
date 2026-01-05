54ed8e14dea6513bb47e9f6fe673630b470aa3f3
Mehtab Hussain <tradercoder786@gmail.com>
2026-01-05 23:17:41 +0530

    redux basic setup

--- mega-blog/src/App.jsx
+++ mega-blog/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/AuthService";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userdata) => {
+        if (userdata) {
+          dispatch(login({ userdata }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return loading ? (
+    <div className="text-2xl text-black font-bold">Loading...</div>
+  ) : (
+    <div className="min-h-screen w-full bg-neutral-800 text-white ">
+      <Header />
+
+      <main>{/* <outlet?> */}</main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
